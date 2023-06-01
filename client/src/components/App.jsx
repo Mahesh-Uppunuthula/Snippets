5dd5d619c0767504ef5e56ecbffd979f60247fa4
Mahesh-Uppunuthula <108076194+Mahesh-Uppunuthula@users.noreply.github.com>
2023-06-02 03:27:54 +0530

    rebuilt after deleting zombie code

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import NavBar from "../components/NavBar";
-
 import Home from "../pages/Home";
 import Dashboard from "../pages/Dashboard";
-import Profie from "../pages/Profile";
 import AuthRegister from "../pages/AuthRegister";
 import AuthLogin from "../pages/AuthLogin";
-
-import ToastMessage from "./ToastMessage/ToastMessage";
 import Editor from "../pages/Editor";
 
 const App = () => {
@@ -20,7 +16,6 @@ const App = () => {
           <Route path="/" Component={Home}></Route>
           <Route path="/dashboard" Component={Dashboard}></Route>
           <Route path="/editor" Component={Editor} ></Route>
-          <Route path="/profile" Component={Profie} ></Route>
           <Route path="/register" Component={AuthRegister} ></Route>
           <Route path="/login" Component={AuthLogin} ></Route>
         </Routes>
