1da1aec2257f2b472c6db2e326a2be325cef51e9
Mahesh-Uppunuthula <uppunuthulamahesh312@gmail.com>
2023-06-25 23:16:52 +0530

    added profile section

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,22 +2,21 @@ import React, { useEffect, useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import ToastMessage from "./ToastMessage/ToastMessage";
 import Axios from "axios";
-import logout_btn from "../Assests/logout.svg"
+import logout_btn from "../Assests/logout.svg";
+import profileIcon from "../Assests/default_profile_icon.svg";
 
 import helper from "../Services/helper";
 const BASE_URL = helper.BASE_URL;
 
 function NavBar() {
-
   const toastRef = useRef(null);
   const navigate = useNavigate();
   const token = window.localStorage.getItem("token");
-  const [userEmail,setUserEmail] = useState("");
+  const [userEmail, setUserEmail] = useState("");
 
   const [isLoggdIn, setLoggedIn] = useState(true);
 
-
-  console.log('BASE URL', BASE_URL);
+  console.log("BASE URL", BASE_URL);
 
   useEffect(() => {
     // console.log("dashboard token", token);
@@ -42,32 +41,46 @@ function NavBar() {
     }
   }, []);
 
-  function logUserOut(event){
-  
+  function logUserOut(event) {
     toastRef.current.toast();
     window.localStorage.removeItem("token");
     setLoggedIn(false);
-    window.location.pathname="/"
-    
+    window.location.pathname = "/";
   }
 
   return (
     <>
-    <header>
-      <nav>
-        <div>
-          <Link className="brand link-item" to="/">
-            Snippets
-          </Link>
-        </div>
-        <div>
-          <ul>
-            {!isLoggdIn ? <button><Link className="link-item" to="/login">Login</Link></button> : <button className="email-btn"  onClick={logUserOut}><p>{userEmail}</p><img src={logout_btn}/></button> }
-          </ul>
-        </div>
-      </nav>
-    </header>
-    <ToastMessage message={"logging you out"} type = {"error"} ref={toastRef}/>
+      <header>
+        <nav>
+          <div>
+            <Link className="brand link-item" to="/">
+              Snippets
+            </Link>
+          </div>
+          <div>
+            <ul>
+              {!isLoggdIn ? (
+                <button>
+                  <Link className="link-item" to="/login">
+                    Login
+                  </Link>
+                </button>
+              ) : (
+                <div className="profile-btn">
+                  <div className="details-section">
+                    <p className="username">Uppunuthula</p>
+                    <p className="profession">student</p>
+                  </div>
+                  <div className="profile-icon">
+                    <img src={profileIcon}/>
+                  </div>
+                </div>
+              )}
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <ToastMessage message={"logging you out"} type={"error"} ref={toastRef} />
     </>
   );
 }
